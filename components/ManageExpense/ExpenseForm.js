68aab6592e9080e8f9985b8302d7fd0b00dcd172
agpavlik <agpavlik@gmail.com>
2024-05-18 23:21:19 -0400

    Add style to form

--- components/ManageExpense/ExpenseForm.js
+++ components/ManageExpense/ExpenseForm.js
@@ -1,4 +1,4 @@
-import { View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 import Input from "./Input";
 
@@ -7,31 +7,55 @@ function ExpenseForm() {
 
   return (
     <View>
-      <Input
-        label="Amount"
-        textInputConfig={{
-          keyboardType: "decimal-pad",
-          onChangeText: amountChangedHandler, // this allows to point at a function that should be executed whenever a user enters a value here.
-        }}
-      />
-      <Input
-        label="Date"
-        textInputConfig={{
-          placeholder: "YYYY-MM-DD",
-          maxLength: 10,
-          onChangeText: () => {},
-        }}
-      />
-      <Input
-        label="Description"
-        textInputConfig={{
-          multiline: true,
-          // autoCapitalize: 'none'
-          // autoCorrect: false // default is true
-        }}
-      />
+      <Text>Your Expense</Text>
+      <View>
+        <Input
+          label="Amount"
+          textInputConfig={{
+            keyboardType: "decimal-pad",
+            onChangeText: amountChangedHandler, // this allows to point at a function that should be executed whenever a user enters a value here.
+          }}
+        />
+        <Input
+          label="Date"
+          textInputConfig={{
+            placeholder: "YYYY-MM-DD",
+            maxLength: 10,
+            onChangeText: () => {},
+          }}
+        />
+        <Input
+          label="Description"
+          textInputConfig={{
+            multiline: true,
+            // autoCapitalize: 'none'
+            // autoCorrect: false // default is true
+          }}
+        />
+      </View>
     </View>
   );
 }
 
 export default ExpenseForm;
+
+// const newLocal = StyleSheet.create({
+//   form: {
+//     marginTop: 40,
+//   },
+//   title: {
+//     fontSize: 24,
+//     fontWeight: "bold",
+//     color: "white",
+//     marginVertical: 24,
+//     textAlign: "center",
+//   },
+//   inputsRow: {
+//     flexDirection: "row",
+//     justifyContent: "space-between",
+//   },
+//   rowInput: {
+//     flex: 1,
+//   },
+// });
+// const styles = newLocal;
--- components/ManageExpense/Input.js
+++ components/ManageExpense/Input.js
@@ -1,12 +1,42 @@
-import { Text, TextInput, View } from "react-native";
+import { Text, TextInput, View, StyleSheet } from "react-native";
+import { GlobalStyles } from "../../constants/styles";
 
-function Input({ label, textInputConfig }) {
+function Input({ label, style, textInputConfig }) {
+  const inputStyles = [styles.input];
+  // Apply to the input if it is multiline
+  if (textInputConfig && textInputConfig.multiline) {
+    inputStyles.push(styles.inputMultiline);
+  }
+
   return (
-    <View>
-      <Text>{label}</Text>
-      <TextInput {...textInputConfig} />
+    <View style={[styles.inputContainer, style]}>
+      <Text style={styles.label}>{label}</Text>
+      <TextInput style={inputStyles} {...textInputConfig} />
     </View>
   );
 }
 
 export default Input;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginHorizontal: 4,
+    marginVertical: 8,
+  },
+  label: {
+    fontSize: 12,
+    color: GlobalStyles.colors.primary100,
+    marginBottom: 4,
+  },
+  input: {
+    backgroundColor: GlobalStyles.colors.primary100,
+    color: GlobalStyles.colors.primary700,
+    padding: 6,
+    borderRadius: 3,
+    fontSize: 18,
+  },
+  inputMultiline: {
+    minHeight: 100,
+    textAlignVertical: "top",
+  },
+});
