d45a28014486836fbe0ff70f7708a969b342319c
agpavlik <agpavlik@gmail.com>
2024-05-20 14:46:08 -0400

    Handling User Input in a Generic Way

--- components/ManageExpense/ExpenseForm.js
+++ components/ManageExpense/ExpenseForm.js
@@ -1,9 +1,28 @@
 import { StyleSheet, Text, View } from "react-native";
 
 import Input from "./Input";
+import { useState } from "react";
 
 function ExpenseForm() {
-  function amountChangedHandler() {}
+  // ---- Handle input
+  const [inputValues, setInputValues] = useState({
+    amount: "",
+    date: "",
+    description: "",
+  });
+
+  // Instead of creation of several handler for every input there is a solution for all the inputs
+  // The 'enteredValue'- is automatic oparameter set by React. An additional parameter "inputIdentifier" allows to call the exact state.
+  // The idea is to overwrite the exact value for the one input and keep all the states that have not changed
+  function inputChangedHandler(inputIdentifier, enteredValue) {
+    setInputValues((curInputValues) => {
+      return {
+        ...curInputValues, // spread all the values
+        [inputIdentifier]: enteredValue, // overwrite one value of amount/date/description
+      };
+    });
+  }
+  // ----
 
   return (
     <View style={styles.form}>
@@ -14,7 +33,8 @@ function ExpenseForm() {
           label="Amount"
           textInputConfig={{
             keyboardType: "decimal-pad",
-            onChangeText: amountChangedHandler, // this allows to point at a function that should be executed whenever a user enters a value here.
+            onChangeText: inputChangedHandler.bind(this, "amount"), // this allows to point at a function that should be executed whenever a user enters a value here. The second argument is "inputIdentifier"
+            value: inputValues.amount,
           }}
         />
         <Input
@@ -23,7 +43,8 @@ function ExpenseForm() {
           textInputConfig={{
             placeholder: "YYYY-MM-DD",
             maxLength: 10,
-            onChangeText: () => {},
+            onChangeText: inputChangedHandler.bind(this, "date"),
+            value: inputValues.date,
           }}
         />
       </View>
@@ -33,6 +54,8 @@ function ExpenseForm() {
           multiline: true,
           // autoCapitalize: 'none'
           // autoCorrect: false // default is true
+          onChangeText: inputChangedHandler.bind(this, "description"),
+          value: inputValues.description,
         }}
       />
     </View>
